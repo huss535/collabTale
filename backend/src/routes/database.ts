98b52b2b572f6d7fe53e28c8b440eb95f44e53ad
efar716 <110000165+efar716@users.noreply.github.com>
2024-01-29 19:11:38 +1300

    backend adding story entries

--- backend/src/routes/database.ts
+++ backend/src/routes/database.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express"
-import { getDocs, collection, getFirestore } from "firebase/firestore";
+import { getDocs, getDoc, doc, collection, getFirestore, updateDoc, arrayUnion } from "firebase/firestore";
 import { story } from "../models/story";
 import { firebaseApp } from "../firebase";
 
@@ -33,5 +33,27 @@ router.get("/stories", async (req: Request, res: Response) => {
 
     res.send(fetchedStories);
 })
+
+router.post("/postStoryEntry", async (req: Request, res: Response) => {
+    let id: any = req.query.id;
+    let newStoryEntry = req.query.newStoryEntry;
+    if (!id) { res.status(400).json({ error: "missing id" }) }
+    const docRef = doc(db, 'story', id.toString());
+    try {
+        await updateDoc(docRef, {
+            storyText: arrayUnion(newStoryEntry), // Add value to the array
+            // yourArrayField: arrayRemove(valueToRemove), // Remove value from the array
+            // If you want to remove a value from the array, uncomment the above line
+        });
+        console.log("Entered");
+        res.end("Updated successfully");
+    /* if (docSnap.exists()) {
+         const data = docSnap.data();
+         return res.status(200).json({ data });
+     } else {
+         return res.status(404).json({ error: 'Document not found.' });
+     }*/} catch (errorMessage) { res.status(400).json({ error: errorMessage }) }
+
+});
 
 module.exports = router;
